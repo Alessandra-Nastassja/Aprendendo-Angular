ef2e5a5c03f23782ec7f66824ee393d68a042342
Alessandra Nastassja <alessandra.nastassja@hotmail.com>
2020-03-02 16:21:34 -0300

    Atualizando repositório

--- PROJETOS/passaro-urbano/src/app/util/rxjs-extencions.ts
+++ PROJETOS/passaro-urbano/src/app/util/rxjs-extencions.ts
@@ -2,4 +2,4 @@ import "rxjs/add/operator/switchMap";
 import "rxjs/add/operator/debounceTime";
 import "rxjs/add/operator/distinctUntilChanged";
 import "rxjs/add/operator/catch";
-import "rxjs/add/Observable/of";
+import "rxjs/add/observable/of";
